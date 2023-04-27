486a0e3b3bb73a3603b4ca7344329934cabcd792
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-04-27 16:15:38 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -4,26 +4,35 @@
 /*Libraries*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include <limits.h>
-#include <signal.h>
+#include <time.h>
 
-/*Functions*/
-char *read_line(void);
-char **splits(char *line, char *delim);
-int execute(char **parse);
-void _puts(char *str);
-int _putchar(char c);
-unsigned int _strlen(char *s);
-char **find_path(char **environ);
-char *_getenv(char **environ, char *dirname);
-char *args_path(char **parse, char **tokens);
-char *_strdup(char *str);
-char *if_exists(char **environ);
+/*Extension Functions*/
 extern char **environ;
+
+/*Function Prototypes*/
+int _strcmp(char *cmd, char *argv);
+int _path(char **strings);
+int _builtins(char **strings, int exit_stat);
+char *_env(char *local_var);
+char *_strdup(char *std);
+char *append_command(char *dir_path, char *instruct);
+int _fprint(const char *prompt, unsigned int sizes);
+void _freed(char *in);
+int _inputs(char *in, char **strings);
+int stat_error(int n);
+int _error(char **strings, int count);
+int _putchar(char c);
+int _file(char *pathfile);
+int _execute(char **strings);
+void _grid(char **grid, int heigth);
+int _strlen(const char *std);
+
 
 #endif
